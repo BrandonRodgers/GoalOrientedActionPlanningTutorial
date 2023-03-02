82dae7cc6ddb0c80f6f38d4d5ee77100e2553af6
Brandon Rodgers <brandonx87@gmail.com>
2023-03-02 15:20:48 -0600

    Current progress on Section 3

---
+++ Assets/Scripts/GAgent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SubGoal
+{
+    public Dictionary<string, int> subGoals;
+    public bool remove;
+
+    public SubGoal(string s, int i, bool r)
+    {
+        subGoals = new Dictionary<string, int>();
+        subGoals.Add(s, i);
+        remove = r;
+    }
+}
+
+public class GAgent : MonoBehaviour
+{
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+
+    GPlanner planner;
+    private Queue<GAction> actionQueue;
+    public GAction currentAction;
+    private SubGoal currentGoal;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach (GAction a in acts)
+        {
+            actions.Add(a);
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+    }
+}
